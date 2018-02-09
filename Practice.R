458d6b641b81dd916f166d1d8a52239b9232cff1
Angeleen Olson <angeleenolson@gmail.com>
2018-02-09 11:30:47 -0800

    Here ya go!

--- Practice.R
+++ Practice.R
@@ -3,3 +3,12 @@ My first commit
 Adding another line 
 
 doing really awesome coding
+
+
+# From Angeleen & Carolyn:
+
+#Q: Why does a flamingo lift up one leg? 
+#A: Because if it lifted both legs it would fall over! 
+
+
+
